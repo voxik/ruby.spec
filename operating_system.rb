1f5c20e17694e77a5bd0e665f52c822953e33b3f
Vít Ondruch <vondruch@redhat.com>
2011-12-06 16:14:47 +0100

    Flexible directory configuration.
    The configuration reflects the configuration parameters and allows to
    specify custom locations if needed.

--- operating_system.rb
+++ operating_system.rb
@@ -1,35 +1,61 @@
 module Gem
   class << self
-    def root_user_dir
-      File.join ['/', 'usr', 'local', 'share', 'gems']
+
+    ##
+    # Returns a string representing that part or the directory tree that is
+    # common to all specified directories.
+
+    def common_path(dirs)
+      paths = dirs.collect {|dir| dir.split(File::SEPARATOR)}
+      uncommon_idx = paths.transpose.each_with_index.find {|dirnames, idx| dirnames.uniq.length > 1}.last
+      paths[0][0 ... uncommon_idx].join(File::SEPARATOR)
+    end
+    private :common_path
+
+    ##
+    # Default gems locations allowed on FHS system (/usr, /usr/share).
+    # The locations are derived from directories specified during build
+    # configuration.
+
+    def default_locations
+      @default_locations ||= {
+        :system => common_path([ConfigMap[:vendorlibdir], ConfigMap[:vendorarchdir]]),
+        :local => common_path([ConfigMap[:sitelibdir], ConfigMap[:sitearchdir]])
+      }
     end
 
-    def root_bindir
-      File.join ['/', 'usr', 'local', 'bin']
+    ##
+    # For each location provides set of directories for binaries (:bin_dir)
+    # and platform independent (:gem_dir) files.
+
+    def default_dirs
+      @default_dir ||= Hash[default_locations.collect do |destination, path|
+          [destination, {
+            :bin_dir => File.join(path, ConfigMap[:bindir].split(File::SEPARATOR).last),
+            :gem_dir => File.join(path, ConfigMap[:datadir].split(File::SEPARATOR).last, 'gems'),
+          }]
+        end]
     end
 
-    def rpm_gem_dir
-      File.join [ConfigMap[:datadir], 'gems']
-    end
+    ##
+    # RubyGems default overrides.
 
     def default_dir
       if Process.uid == 0
-        Gem.root_user_dir
+        Gem.default_dirs[:local][:gem_dir]
       else
         Gem.user_dir
       end
     end
 
     def default_path
-      path = []
-      path << Gem.user_dir if File.exist? Gem.user_home
-      path << Gem.root_user_dir
-      path << Gem.rpm_gem_dir
+      path = default_dirs.collect {|location, paths| paths[:gem_dir]}
+      path.unshift Gem.user_dir if File.exist? Gem.user_home
     end
 
     def default_bindir
       if Process.uid == 0
-        File.join ['/', 'usr', 'local', 'bin']
+        Gem.default_dirs[:local][:bin_dir]
       else
         File.join [Dir.home, 'bin']
       end
